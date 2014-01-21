d08c53b2c42503139278dc410f5d81b3863ec4e1
Tim-Feng <fsjforever26@gmail.com>
2014-01-21 16:09:51 +0800

    modify migration to change format

--- db/migrate/20140121072700_change_post_id_format_in_post_categories.rb
+++ db/migrate/20140121072700_change_post_id_format_in_post_categories.rb
@@ -1,6 +1,6 @@
 class ChangePostIdFormatInPostCategories < ActiveRecord::Migration
   def change
-    change_column :post_categories, :post_id, :integer
-    change_column :post_categories, :category_id, :integer
+    change_column :post_categories, :post_id, 'integer USING CAST(post_id AS integer)'
+    change_column :post_categories, :category_id, 'integer USING CAST(category_id AS integer)'
   end
 end
