5edb92e57eba746d3043d8ef93a62a7f05133ca7
Tim-Feng <fsjforever26@gmail.com>
2014-01-21 13:45:35 +0800

    GRAVATAR

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, on: :create, length: {minimum: 5}
+  validates :github_link, uniqueness: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
 
   sluggable_column :username
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140117030950) do
+ActiveRecord::Schema.define(version: 20140121035657) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 20140117030950) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "slug"
+    t.string   "github_link"
+    t.string   "email"
   end
 
 end
